eae7d7eb9b305ab9e59745be666f962c7ffb4a09
Dmitriy Zaitsev <dimyantis@gmail.com>
2024-09-24 13:56:40 -0700

    Begin work (first point)

---
+++ Lesson_trening3.py
@@ -0,0 +1,19 @@
+print('Hello World')
+message = 'Hello World again'
+print(message)
+message=("'Hello World again'")
+print(message)
+first_name='dmitriy'
+last_name='zaitsev'
+print(first_name .title()+ ' ' + last_name .title())
+print('')
+lang_1='\nPython\n'
+lang_2='C++\n'
+lang_3='Basic'
+print('Program languages',lang_1 +' '+lang_2 +' '+lang_3)
+name='Carl jon'
+initials=name [0] + name[5]
+print(name[0])
+print(name[0:4])
+print(name[0:4] +' '+ initials + ' ' + name[5: len(name)])
+
