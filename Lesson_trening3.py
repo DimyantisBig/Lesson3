5fda2ee5637d2da54360a407c9982248b71d11c5
Dmitriy Zaitsev <dimyantis@gmail.com>
2024-09-24 15:04:35 -0700

    Add 3 files

--- Lesson_trening3.py
+++ Lesson_trening3.py
@@ -10,7 +10,7 @@ print('')
 lang_1='\nPython\n'
 lang_2='C++\n'
 lang_3='Basic'
-print('Program languages',lang_1 +' '+lang_2 +' '+lang_3)
+print('Program languages' , lang_1 +' '+lang_2 +' '+lang_3)
 name='Carl jon'
 initials=name [0] + name[5]
 print(name[0])
