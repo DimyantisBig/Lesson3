d304411eb4dda87ba176f9b23e0bf4eecf6eec6c
Dmitriy Zaitsev <dimyantis@gmail.com>
2024-09-27 12:58:05 -0700

    Add new files

---
+++ Lesson_text.py
@@ -0,0 +1,13 @@
+from operator import index
+
+text = 'hello'
+print(text.index('e'))
+x = 'world'
+print(x.index('r'))
+message = "Hello beautiful world!"
+final_message = message[0:5] + " " + message[-6:] + " Today is a " + message[6:15] + " day."
+print(final_message)
+number = 57
+number = 'fiftyseven'
+print(number)
+
---
+++ my_first_prog.py
@@ -0,0 +1,5 @@
+print('My first programm'.upper())
+price = 88
+amount = 100
+total = price * amount
+print('Total value :' , total)
