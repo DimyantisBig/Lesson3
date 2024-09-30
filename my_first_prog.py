5cf6edf835a45bf7f736ceb7637756867084be16
Dmitriy Zaitsev <dimyantis@gmail.com>
2024-09-30 13:39:47 -0700

    I write my first program!I licked!

--- my_first_prog.py
+++ my_first_prog.py
@@ -1,5 +1,15 @@
+
 print('My first programm'.upper())
-price = 88
-amount = 100
-total = price * amount
-print('Total value :' , total)
+price = float(input('Add your price: '))
+summ = float(input('Add your summ: '))
+
+sum_result = price + summ
+sub_result = price - summ
+mul_result = price * summ
+div_result = price / summ if summ !=0 else 'Division by 0 is impossible'
+
+print(f'Total plus : {sum_result}')
+print(f'Total minus : {sub_result}')
+print(f'Total multipliy : {mul_result}')
+print(f'Total split : {div_result}')
+
